2a1fdd86e7cfff7be7037b33ab1d33d04ce0cf27
Clive Dias <clive.dias@netcracker.com>
2025-09-15 14:03:40 +0530

    Add import

--- acl-configurator/consul-acl-configurator-operator/controllers/consulacl_controller.go
+++ acl-configurator/consul-acl-configurator-operator/controllers/consulacl_controller.go
@@ -33,6 +33,7 @@ import (
 	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
