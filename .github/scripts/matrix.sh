ed3493ee4ae417702dd3baa4d5000794a6389091
Boris Lavrishchev <boris.lavrishchev.qubership@gmail.com>
2025-12-16 15:16:15 +0300

    fix: fixed linters

--- .github/scripts/matrix.sh
+++ .github/scripts/matrix.sh
@@ -8,12 +8,12 @@ files_file="$2"
 # Use jq to filter components where any file starts with any changeset entry in the list
 # shellcheck disable=2086
 jq -c --argjson files "$(cat $files_file)" '
-  .components |
-  [ .[] | select(
-      . as $component |
-      any($files[];
-        [ $component.changeset[] as $ch | startswith($ch) ] | any
-      )
+    .components |
+    [ .[] | select(
+    . as $component |
+    any($files[];
+    [ $component.changeset[] as $ch | startswith($ch) ] | any
     )
-  ]
+    )
+    ]
 ' "$components_file"
